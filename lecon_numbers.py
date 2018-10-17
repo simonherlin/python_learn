c782c7430c2689212e466f6c4dfb78ef631d2dba
simon <simon@debian.debian_simon>
2018-10-17 07:16:51 +0200

    [FIX] name file number, some problem with the name of the fils, and finish the lecon

--- lecon_numbers.py
+++ lecon_numbers.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 # int
 my_int = 6
 print('value: {}, type: {}'.format(my_int, type(my_int)))
@@ -18,3 +20,16 @@ print(val)
 print(7 // 5)
 print(7 % 5)
 print(7 ** 5)
+
+# Decimal
+from_float = Decimal(0.1)
+from_str = Decimal('0.1')
+print('from float: {}\nfrom string: {}'.format(from_float, from_str))
+my_decimal = Decimal('0.1')
+sum_of_decimals = my_decimal + my_decimal + my_decimal
+print(sum_of_decimals == Decimal('0.3'))
+
+# Operator precedence in calculations
+
+print(1 + 2**2 * 3 / 6) # 1 + 4 * 3 / 6 == 1 + 12 / 6 == 1 + 2
+print((1 + 2**2) * 3 / 6)
